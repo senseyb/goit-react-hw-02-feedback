df886542bcc472280ea2035622ef4e746239822e
senseyb <senseyb7@gmail.com>
2021-06-14 22:55:42 +0300

    Update Notification.js

--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -5,10 +5,6 @@ import styles from './Notification.module.css';
 const Notification = ({ message }) => {
   return <p className={styles.text}>{message}</p>;
 };
-
-Notification.defaultProps = {
-    message: 'No feedbacks yet',
-  };
   
   Notification.propTypes = {
     message: PropTypes.string.isRequired,
